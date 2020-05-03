e8129adc088873c6ca89037d413d3e3a39481dc6
bencelaszlo <bencelaszlo@protonmail.com>
2020-05-03 03:14:15 +0200

    Require dependencies, configure server.
    On branch master
    
    Changes to be committed:
    	modified:   server.js

--- server.js
+++ server.js
@@ -1,20 +1,30 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
-// Require Express to run server and routes
+// Require Express, CORS and Body Parser
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+// Configure the server port and cross origin allowance
+const port = 3000;
+const constOptions = {
+    origin: '*',
+    optionSuccessStatus: 200
+};
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
+// Configure body-parser as middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-// Cors for cross origin allowance
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+app.listen(port, () => {
+    console.log(`Server is listening on ${port}`);
+});
