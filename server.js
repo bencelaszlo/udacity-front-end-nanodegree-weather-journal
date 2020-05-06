5e0bc34bfeb173ee917427581145acbe583299bb
bencelaszlo <bencelaszlo@protonmail.com>
2020-05-06 21:06:20 +0200

    Implement weather querying and UI updating. Format source code.
    On branch master
    
    Changes to be committed:
    	modified:   server.js
    	modified:   website/app.js
    	modified:   website/index.html
    	modified:   website/style.css

--- server.js
+++ server.js
@@ -7,24 +7,52 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 // Configure the server port and cross origin allowance
-const port = 3000;
-const constOptions = {
-    origin: '*',
-    optionSuccessStatus: 200
-};
+const PORT = 3000;
 
 // Start up an instance of app
 const app = express();
 
 /* Middleware*/
 // Configure body-parser as middleware
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
 app.use(bodyParser.json());
+
+app.use(cors())
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 // Setup Server
-app.listen(port, () => {
-    console.log(`Server is listening on ${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is listening on ${PORT}`);
+});
+
+app.get('/all', (req, res) => {
+    try {
+        res.status(200).send(projectData);
+    } catch (error) {
+        console.error(JSON.stringify(error));
+        res.status(500).send();
+    }
+});
+
+app.post('/', (req, res) => {
+    try {
+        const { temperature, date, userResponse } = req.body;
+        if (!(temperature && date && userResponse)) {
+            throw new Error('temperature, date and userResponse are required fields');
+        }
+        projectData = {
+            temperature: temperature,
+            date: date,
+            userResponse: userResponse
+        };
+        res.status(200).send();
+    } catch (error) {
+        console.error(JSON.stringify(error));
+        res.status(500).send(error);
+    }
 });
--- website/app.js
+++ website/app.js
@@ -1,5 +1,59 @@
 /* Global Variables */
+const API_KEY = '2897c174293ae3e8006d0abfe1a402e4';
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+const generateElement = document.querySelector('#generate');
+
+
+
+const getWeather = async (baseUrl, zipCode, apiKey) => {
+    const response = await fetch(`${baseUrl}?zip=${zipCode}&appid=${apiKey}`, {
+        method: 'GET'
+    });
+    return response.json();
+};
+
+const saveWeather = async (path, data) => {
+    await fetch(path, {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+        body: JSON.stringify(data)
+    });
+    return;
+};
+
+const getAllData = async (path) => {
+    const response = await fetch(path, {
+        method: 'GET'
+    });
+    return response.json();
+}
+
+generateElement.addEventListener('click', () => {
+    getWeather(BASE_URL, document.querySelector('#zip').value, API_KEY).then((weather) => {
+        // Create a new date instance dynamically with JS
+        const d = new Date();
+        const newDate = `${d.getMonth()}.${d.getDate()}.${d.getFullYear()}`;
+
+        const data = {
+            temperature: weather.main.temp,
+            date: newDate,
+            userResponse: document.querySelector('#feelings').value
+        };
+
+        saveWeather('http://localhost:3000/', data).then(() => {
+            getAllData('http://localhost:3000/all').then((storedData) => {
+                document.querySelector('#date').innerHTML = `<span>${storedData.date}</span>`;
+                document.querySelector('#temp').innerHTML = `<span>${storedData.temperature}</span>`;
+                document.querySelector('#content').innerHTML = `<span>${storedData.userResponse}</span>`;
+            });
+        });
+    });
+});
